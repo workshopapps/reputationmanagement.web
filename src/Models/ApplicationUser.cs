191bd7ab133b5f11a544877711e1821e242723fa
David Okeke <davidokeke.c@gmail.com>
2022-11-16 16:16:50 +0100

    Git commit -m "Initial Commit"

---
+++ src/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace src.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+
+        public string PostAddress { get; set; }
+        
+    }
+}
---
+++ src/Services/IReviewRepository.cs
@@ -0,0 +1,15 @@
+﻿using src.Models;
+
+namespace src.Services
+{
+    public interface IReviewRepository
+    {
+        public IQueryable<Review> Reviews { get; }
+
+        Review GetReviewById(Guid id);
+
+        IEnumerable<Review> GetReviews(int pageNumber=0, int pageSize=0);
+
+        // Add more CRUD
+    }
+}
