f405fee01cc51581611599bf438f3e502002760a
Dimeji <adedimeji88@yahoo.com>
2022-11-17 10:04:03 +0100

    added a dto folder and a response dto

---
+++ src/Dto's/ResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace src.Dto_s
+{
+    public class ResponseDto<T>
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
