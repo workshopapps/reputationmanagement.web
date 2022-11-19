5ec4aa198414ef683373024d1ef193849f44d411
dyCodes <yusufdaudu51@gmail.com>
2022-11-19 06:12:42 +0100

    feat: Update file structure

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
-  return <footer>
-            <h4>This is the footer.......write some code bruv</h4>
-        </footer>
+	return (
+		<footer className="bg-gray-500 mt-5 p-3">
+			<h4>This is the footer.......write some code bruv</h4>
+		</footer>
+	);
 }
 export default Footer;
--- frontend/src/pages/LandingPage/index.jsx
+++ frontend/src/pages/LandingPage/index.jsx
@@ -1,34 +1,34 @@
-import React from "react";
-import PageLayout from "../../layout/pageLayout";
+import React from 'react';
+import PageLayout from '../../layout/PageLayout';
 import {
-  HeroSection,
-  HowDoesItWork,
-  StyledLandingPage,
-} from "./LandingPage.styled";
+	HeroSection,
+	HowDoesItWork,
+	StyledLandingPage,
+} from './LandingPage.styled';
 
 const LandingPage = () => {
-  return (
-    <PageLayout>
-      <StyledLandingPage>
-        <HeroSection>
-          <h1>
-            We help you maintain your <span>brand’s</span> reputation
-          </h1>
-          <p>
-            Get your business to the next level, by removing every bad review
-            that is getting in the way, our lawyers are here to help you
-            maintain your brand’s reputation.
-          </p>
+	return (
+		<PageLayout>
+			<StyledLandingPage>
+				<HeroSection>
+					<h1>
+						We help you maintain your <span>brand’s</span> reputation
+					</h1>
+					<p>
+						Get your business to the next level, by removing every bad review
+						that is getting in the way, our lawyers are here to help you
+						maintain your brand’s reputation.
+					</p>
 
-          <div className="hero-button">Get a quote</div>
-        </HeroSection>
+					<div className="hero-button">Get a quote</div>
+				</HeroSection>
 
-        <HowDoesItWork>
-          <h1>How does it work?</h1>
-        </HowDoesItWork>
-      </StyledLandingPage>
-    </PageLayout>
-  );
+				<HowDoesItWork>
+					<h1>How does it work?</h1>
+				</HowDoesItWork>
+			</StyledLandingPage>
+		</PageLayout>
+	);
 };
 
 export default LandingPage;
