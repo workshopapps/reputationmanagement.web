66d2de78cdf85b2f701112af64195134af5db6e9
dyCodes <yusufdaudu51@gmail.com>
2022-11-19 13:59:02 +0100

    feat: Update LandingPage "hero section"

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home/Home';
 import LandingPage from '../pages/LandingPage';
 import WeRemoveGoogleSearch from '../pages/WeRemoveGoogleSearch/WeRemoveGoogleSearch';
 
@@ -7,9 +6,11 @@ const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="landing-page" element={<LandingPage />} />
-				<Route path="we-remove-google-search" element={<WeRemoveGoogleSearch />} />
+				<Route path="/" element={<LandingPage />} />
+				<Route
+					path="we-remove-google-search"
+					element={<WeRemoveGoogleSearch />}
+				/>
 			</Routes>
 		</BrowserRouter>
 	);
