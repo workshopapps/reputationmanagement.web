3e2b275414902512c1ba4647de557686a1219fc8
John <John@Johns-MacBook-Pro.local>
2022-11-19 20:44:12 +0100

    adding states for active successful request Modal

--- frontend/src/context/appContext.js
+++ frontend/src/context/appContext.js
@@ -3,9 +3,10 @@ import { createContext, useState } from "react";
 export const AppContext = createContext();
 
 const AppProvider = ({children}) => {
-    const [ someState, setSomeState ] = useState(0)
+    const [ someState, setSomeState ] = useState(0);
+    const [ requestSuccessfulModalActive, setRequestSuccessfulModalActive ] = useState(true)
     return(
-        <AppContext.Provider value={{someState, setSomeState}}>
+        <AppContext.Provider value={{someState, setSomeState, requestSuccessfulModalActive, setRequestSuccessfulModalActive}}>
             {children}
         </AppContext.Provider>
     )
