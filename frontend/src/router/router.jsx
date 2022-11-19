2d70212f1e8559bd1d582c8794dc499edb2db66e
dyCodes <yusufdaudu51@gmail.com>
2022-11-19 04:17:21 +0100

    feat: Add landing page

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from '../pages/home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/home";
+import LandingPage from "../pages/LandingPage";
 
 const Router = () => {
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="landing-page" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 export default Router;
