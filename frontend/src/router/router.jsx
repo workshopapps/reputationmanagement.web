797c0bfb6d61782b756658644d627eb6d394cd44
manaboy-07 <manassehoruebor@gmail.com>
2022-11-19 14:25:27 +0100

    feat(FE-31):added carrerpg-1 still workuing on the styling

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Carrerpg1 from '../pages/Carrer/Carrerpg1';
 import Home from '../pages/Home/Home';
 import LandingPage from '../pages/LandingPage';
 import WeRemoveGoogleSearch from '../pages/WeRemoveGoogleSearch/WeRemoveGoogleSearch';
@@ -9,7 +10,11 @@ const Router = () => {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="landing-page" element={<LandingPage />} />
-				<Route path="we-remove-google-search" element={<WeRemoveGoogleSearch />} />
+				<Route
+					path="we-remove-google-search"
+					element={<WeRemoveGoogleSearch />}
+				/>
+				<Route path="carrer-pg-1" element={<Carrerpg1 />} />
 			</Routes>
 		</BrowserRouter>
 	);
