d9e69b1eeba90184da3c63b11b5d15eabe0bd8a1
DevOyinda <kudiratoyindamolad@gmail.com>
2022-11-19 14:29:26 +0100

    cloned the development repo, pulled from development re and created feat/BAC-13-return-successful-resolved-cases create a Get successfulTeviews class in the Dtos model created a GetAllSuccessfulReview interface and implemented it in the AzSqlReviewRepo class created a successreview endpoint in the Lawyer Controller

--- src/Services/IReviewRepository.cs
+++ src/Services/IReviewRepository.cs
@@ -17,5 +17,6 @@ namespace src.Services
 
         // Add more CRUD
         Review UpdateReviewLawyer(ReviewForUpdateDTO review);
+        Task<List<GetSuccessfulReviews>> GetAllSuccessfulReview();
     }
 }
