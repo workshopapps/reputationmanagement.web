e45c30309d1ff1e57a4e872a44d0015fac6e1e8e
Tayor99 <adenusiadetayo@gmail.com>
2022-11-19 21:09:54 +0100

    feat(FRO-22): added pagehero to the glassdoor page

--- frontend/src/layout/PageLayout.js
+++ frontend/src/layout/PageLayout.js
@@ -2,11 +2,10 @@ import Header from '../components/Layout/Header';
 import Footer from '../components/Reusables/FooterComponents/ReUsableFooter';
 import RequestSuccessful from '../modal/request-successful/requestSuccessful';
 
-
 const PageLayout = ({ children }) => {
 	return (
 		<main>
-			<RequestSuccessful/>
+			{/* <RequestSuccessful/> */}
 			<Header />
 			{children}
 			<Footer />
--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -4,10 +4,9 @@ import AboutPage from '../pages/AboutUs/AboutPage';
 
 import Carrerpg1 from '../pages/Carrer/Carrerpg1';
 
-import Home from '../pages/Home/Home';
+// import Home from '../pages/Home/Home';
 
 import Faqs from '../pages/FAQ/Faqs';
-import Carrerpg1 from '../pages/Carrer/Carrerpg1';
 
 import LandingPage from '../pages/LandingPage';
 import RequestSuccessfulSm from '../pages/Request Successful/requestSuccessful';
@@ -17,13 +16,13 @@ import WeRemoveGoogleSearch from '../pages/WeRemoveGoogleSearch/WeRemoveGoogleSe
 
 import Privacy from '../pages/Privacy Policy/Privacy';
 import Blog from '../pages/Blog/Blog';
-import Faqs from '../pages/FAQ/Faqs';
+import GlassDoor from '../pages/GlassDoor/GlassDoor';
 
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				{/* <Route path="/" element={<Home />} /> */}
 				<Route path="landing-page" element={<LandingPage />} />
 				<Route path="about-us" element={<AboutPage />} />
 
@@ -38,7 +37,7 @@ const Router = () => {
 					path="we-remove-google-review"
 					element={<WeRemoveGoogleReview />}
 				/>
-				<Route path='/request-successful' element={<RequestSuccessfulSm/>}/>
+				<Route path="/request-successful" element={<RequestSuccessfulSm />} />
 
 				<Route path="terms-of-use" element={<Termsofuse />} />
 
@@ -49,6 +48,7 @@ const Router = () => {
 				<Route path="FAQ" element={<Faqs />} />
 
 				<Route path="carrer-pg-1" element={<Carrerpg1 />} />
+				<Route path="glassdoor" element={<GlassDoor />} />
 			</Routes>
 		</BrowserRouter>
 	);
