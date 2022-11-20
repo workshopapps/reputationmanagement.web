298595bce6668a22058e3991e9a25904b1facfe6
Bunde Uji <bundeuji@gmail.com>
2022-11-20 10:40:56 +0100

    chore

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -11,14 +11,51 @@ import Termsofuse from '../pages/TermsOfUse/termsofuse';
 import WeRemoveGoogleReview from '../pages/WeRemoveGoogleReview/WeRemoveGoogleReview';
 import WeRemoveGoogleSearch from '../pages/WeRemoveGoogleSearch/WeRemoveGoogleSearch';
 import LawyerDashboard from '../pages/LawyerDashboard/LawyerDashboard';
+import PrivacyPolicy from '../pages/PrivacyPolicy/PrivacyPolicy';
+import Blog from '../pages/Blog/Blog';
+import GlassDoor from '../pages/GlassDoor/GlassDoor';
+
+import Signup from '../Sign/Signup';
+import ErrorPage from '../pages/ErrorPage';
 
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
+			<Route path="/" element={<LandingPage />} />
+				<Route path="/signup" element={<Signup />} />
+
 				<Route path="landing-page" element={<LandingPage />} />
-				<Route path="we-remove-google-search" element={<WeRemoveGoogleSearch />} />
+
+				<Route path="about-us" element={<AboutPage />} />
+
+				<Route
+					path="we-remove-google-search"
+					element={<WeRemoveGoogleSearch />}
+				/>
+
+				<Route
+					path="we-remove-google-review"
+					element={<WeRemoveGoogleReview />}
+				/>
+				<Route path="/request-successful" element={<RequestSuccessfulSm />} />
+
+				<Route path="terms-of-use" element={<Termsofuse />} />
+
+				<Route path="privacy" element={<PrivacyPolicy />} />
+
+				<Route path="career" element={<Carrerpg1 />} />
+				<Route path="carrer-pg-3" element={<Carrerpg3 />} />
+				<Route path="carrer-pg-2" element={<Carrerpg2 />} />
+
+				<Route path="FAQ" element={<Faqs />} />
+
+				<Route path="glassdoor" element={<GlassDoor />} />
+
+
+				<Route path="blog" element ={<Blog />} />
+				<Route path="*" element ={<ErrorPage />} />
+
 				<Route path='/lawyer-dashboard' element={<LawyerDashboard />} />
 			</Routes>
 		</BrowserRouter>
