a829c0a5565bd9db16e78e0845fb54c8182fb96d
Hammed Akinyemi <hakra911@gmail.com>
2022-11-20 07:31:53 +0100

    feat(FRO-21): add a we remove google review page

--- frontend/src/pages/WeRemoveGoogleReview/WeRemoveGoogleReview.jsx
+++ frontend/src/pages/WeRemoveGoogleReview/WeRemoveGoogleReview.jsx
@@ -1,10 +1,4 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap';
-// import 'bootstrap/dist/css/bootstrap.css';
 
 import WRGRAbout from '../../components/WeRemoveGoogleReview/WRGRAbout';
 import WRGRFooter from '../../components/WeRemoveGoogleReview/WRGRFooter';
