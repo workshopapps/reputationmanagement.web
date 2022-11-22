066fdb3c6f7b902917b806b0feb3e159365ef9a7
John <John@Johns-MacBook-Pro.local>
2022-11-22 16:45:19 +0100

    setting up test with jest and react-testing library

--- frontend/src/test/setupTests.js
+++ frontend/src/test/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{js,jsx}'],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
