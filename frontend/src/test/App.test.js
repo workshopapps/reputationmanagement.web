e068562941efbd17fefc8d8465486acacfd31a42
PriscaTonia <priscaebube@gmail.com>
2022-11-25 12:07:10 +0100

    test(F-13): Adding a test for the app.jsx file

--- frontend/src/test/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import App from '../app/app'
-import React from 'react'
-
-test('App renders successfully without crashing', () => {
-  render(<App />);
-});
