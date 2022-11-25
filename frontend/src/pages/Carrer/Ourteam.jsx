c963eaaf9b52bb0029b978b1a46ab32a69437a2b
SolomonWole <solomonwolemail@gmail.com>
2022-11-25 11:17:47 +0100

    feat(FE-41): Our team page

--- frontend/src/pages/Carrer/Ourteam.jsx
+++ frontend/src/pages/Carrer/Ourteam.jsx
@@ -16,39 +16,14 @@ import okeke from '../../assets/images/blog_images/images/okeke.png';
 import hammed from '../../assets/images/blog_images/images/hammed.png';
 import vivian from '../../assets/images/blog_images/images/vivian.png';
 import adaeze from '../../assets/images/blog_images/images/adaeze.png';
-import ehmee from '../../assets/images/blog_images/images/Ehmee.jpg';
+import ehmee from '../../assets/images/blog_images/images/ehmee.png';
+import solomon from '../../assets/images/blog_images/images/solomon.png';
+import patrick from '../../assets/images/blog_images/images/patrick.png';
+import john from '../../assets/images/blog_images/images/john.png';
 import teamfooter from '../../assets/images/blog_images/images/teamfooter.jpg';
 import TeamTemplate from './TeamTemplate';
 
-const StyledHeader = styled.h1`
-	color: #000;
 
-	font-size: 2rem;
-	padding: 4px 0;
-`;
-const StyledBackGround = styled.section`
-	background: linear-gradient(
-		0deg,
-		rgba(255, 246, 231, 0.91),
-		rgba(255, 246, 231, 0.91)
-	);
-	background-image: url(${Background1});
-`;
-const StyledBanner = styled.div`
-	width: 90%;
-	background-color: #3844a3;
-	height: 200px;
-	border-radius: 8px;
-	margin: 50px 0 50px 0;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	text-align: center;
-	flex-direction: column;
-	// padding: 0px 35px 10px 35px;
-	padding: 30px 150px;
-	color: #fff;
-`;
 const StyledHeader1 = styled.h1`
 	font-size: 30px;
 	font-weight: bold;
@@ -61,21 +36,6 @@ const StyledTeam = styled.span`
 	color: #fc9a4a;
 `;
 
-const StyledMainDiv = styled.div`
-   display: flex;
-   flex-direction: column;
-   align-items: center;
-   justify-content: center:
-`;
-
-const StyledPostSnippet = styled.div`
-	display: flex;
-	width: 90%;
-	gap: 10px;
-	margin: 0 auto;
-	flex-wrap: wrap;
-	line-height: 50px;
-`;
 
 // -------SolomonWole work -------
 const StyledBg = styled.div`
@@ -138,15 +98,33 @@ const TeamFooter = styled.div`
 		margin-bottom: 30px;
 	}
 
-    @media screen and (min-width:768px) {
+    @media screen and (min-width:589px) {
         .footImg {
-            height: 35vh;
+            height: 300px;
             min-width: 100%;
         }
     }
-    @media screen and (max-width:428px) {
+    @media screen and (max-width:584px) {
         .footImg {
-            height: 40vh;
+            height: 350px;
+            min-width: 100%;
+        }
+    }
+    @media screen and (max-width:433px) {
+        .footImg {
+            height: 380px;
+            min-width: 100%;
+        }
+    }
+    @media screen and (max-width:355px) {
+        .footImg {
+            height: 400px;
+            min-width: 100%;
+        }
+    }
+    @media screen and (max-width:307px) {
+        .footImg {
+            height: 450px;
             min-width: 100%;
         }
     }
@@ -173,179 +151,6 @@ const HeadBg = styled.div`
     }
     
 `;
-
-const TeamContainer = styled.div`
-
-@media screen and (min-width:1440px) {
-    max-width: 1024px;
-}
-`
-    
-// function Ourteam() {
-// 	const [teamTemplate] = useState([
-// 		{
-// 			image: mark,
-// 			name: 'Mark Essien',
-// 			role: 'Founder',
-// 			id: 1,
-// 		},
-// 		{
-// 			image: david,
-// 			name: 'David Abraham',
-// 			role: 'Director',
-// 			id: 2,
-// 		},
-// 		{
-// 			image: etim,
-// 			name: 'Idaresit Etim',
-// 			role: 'Manager',
-// 			id: 3,
-// 		},
-// 		{
-// 			image: anidi,
-// 			name: 'Anidi Izuchukwu',
-// 			role: 'Design Lead',
-// 			id: 4,
-// 		},
-// 		{
-// 			image: okeke,
-// 			name: 'David Okeke',
-// 			role: 'Backend Lead',
-// 			id: 5,
-// 		},
-
-// 		{
-// 			image: hammed,
-// 			name: 'Hammed Akinyemi',
-// 			role: 'Frontend Lead',
-// 			id: 6,
-// 		},
-// 		{
-// 			image: vivian,
-// 			name: 'Vivianne Thomas',
-// 			role: 'Sales & Marketing Lead',
-// 			id: 7,
-// 		},
-
-// 		{
-// 			image: adaeze,
-// 			name: 'Adaeze Ifeanyi',
-// 			role: 'Product Manager',
-// 			id: 8,
-// 		},
-// 		{
-// 			image: ehmee,
-// 			name: 'Iniemem Udosen',
-// 			role: 'DevOps',
-// 			id: 9,
-// 		},
-// 	]);
-
-// 	let display;
-// 	if (window.innerWidth < 768) {
-// 		display = false;
-// 	} else {
-// 		display = true;
-// 	}
-// 	return (
-// 		<div className="carrer-landing-pg py-12">
-// 			<PageLayout>
-// 				<StyledMainDiv>
-// 					<StyledBanner>
-// 						<StyledHeader1>
-// 							{' '}
-// 							Meet our <StyledTeam> Team </StyledTeam>
-// 						</StyledHeader1>
-// 						Businesses are at the mercy of negative reviews. Words can have
-// 						devastating impact on brand reputation and can affect future sales
-// 						and recruitment efforts. Here at Fixit, we provide quick and
-// 						reliable actions to erase negative reviews. Here are the key members
-// 						that make up the team.
-// 					</StyledBanner>
-
-// 					<TeamTemplate teamTemplate={teamTemplate} />
-
-// 					<StyledPostSnippet>
-// 						<PostSnippettt
-// 							img={mark}
-// 							title="Mark Essien"
-// 							subtitle="Founder jhgioyo"
-// 							display={display}
-// 						/>
-// 						<PostSnippettt
-// 							img={david}
-// 							title="David Abraham"
-// 							subtitle="Director"
-// 							display={display}
-// 						/>
-// 						{window.innerWidth > 768 && (
-// 							<PostSnippettt
-// 								img={etim}
-// 								title="Idaresit Etim"
-// 								subtitle="Managerr"
-// 								display={display}
-// 							/>
-// 						)}
-
-// 						<PostSnippettt
-// 							img={anidi}
-// 							title="Anidi Izuchukwu"
-// 							subtitle="Design Lead"
-// 							display={display}
-// 						/>
-// 						<PostSnippettt
-// 							img={okeke}
-// 							title="David Okeke"
-// 							subtitle="Backend Lead,"
-// 							display={display}
-// 						/>
-// 						{window.innerWidth > 768 && (
-// 							<PostSnippettt
-// 								img={hammed}
-// 								title="Hammed Akinyemi"
-// 								subtitle="Frontend Lead"
-// 								display={display}
-// 							/>
-// 						)}
-
-// 						<PostSnippettt
-// 							img={vivian}
-// 							title="Vivianne Thomas"
-// 							subtitle="Sales & Marketing Lead"
-// 							display={display}
-// 						/>
-// 						<PostSnippettt
-// 							img={adaeze}
-// 							title="Adaeze Ifeanyi"
-// 							subtitle="product Manager"
-// 							display={display}
-// 						/>
-// 					</StyledPostSnippet>
-// 					<StyledBackGround className="grow-together flex flex-col items-center justify-center py-12 w-full mt-20">
-// 						<StyledHeader className="text-center">
-// 							Want to be a part of something special?
-// 						</StyledHeader>
-// 						<h4 className="mt-3 text-center">
-// 							Brand reputation management is our core responsibility and this is
-// 							made possible with a great team. <br /> There really is no company
-// 							like Fixit, and you can join us
-// 						</h4>
-// 						<Link
-// 							to="/carrer-pg-2"
-// 							onClick={() => {
-// 								window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-// 							}}
-// 						>
-// 							<StyledButton className="mt-4">Join our team</StyledButton>
-// 						</Link>
-// 					</StyledBackGround>
-// 				</StyledMainDiv>
-// 			</PageLayout>
-// 		</div>
-// 	);
-// }
-
-// export default Ourteam;
 
 function Ourteam() {
 	const [teamTemplate] = useState([
@@ -383,7 +188,7 @@ function Ourteam() {
 		{
 			image: hammed,
 			name: 'Hammed Akinyemi',
-			role: 'Frontend Lead',
+			role: 'Frontend',
 			id: 6,
 		},
 		{
@@ -405,9 +210,27 @@ function Ourteam() {
 			role: 'DevOps',
 			id: 9,
 		},
+		{
+			image: john,
+			name: 'John Orisanwo',
+			role: 'Frontend Lead',
+			id: 10,
+		},
+		{
+			image: solomon,
+			name: 'Solomon Adewole',
+			role: 'Frontend Asst. Lead',
+			id: 11,
+		},
+		{
+			image: patrick,
+			name: 'Sinachi Patrick',
+			role: 'Frontend PM',
+			id: 12,
+		},
 	]);
 	return (
-        <TeamContainer>
+        <>
 		<PageLayout>
 			<StyledBg>
             <StyledContainer>
@@ -444,7 +267,7 @@ function Ourteam() {
 					</div>
 				</TeamFooter>
 			</StyledBg>
-		</PageLayout></TeamContainer>
+		</PageLayout></>
 	);
 }
 
