64dd31f1406925aa430d3fecfddac2aa7c2c04e3
John <johnorisanwo@gmail.com>
2022-11-25 14:41:59 +0100

    fixing file structure for test

--- frontend/src/test/App.test.js
+++ frontend/src/test/App.test.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react';
-import App from './app.jsx';
+import App from '../app/app.jsx';
 
 test('should rener app successfully', () => {
-	window.scrollTo = jest.fn();
 	render(<App />);
 });
