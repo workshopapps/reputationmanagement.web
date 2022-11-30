e5dc8b266acb1f91b56fb8f92d3cefbd0df291e3
PriscaTonia <priscaebube@gmail.com>
2022-11-30 01:55:24 +0100

    fix/FE-77: Fixed the width, spacing and typographical errors in the privacy policy page. Edited the button and redesigned it to match the design on figma

--- frontend/src/pages/PrivacyPolicy/Bottom.styled.jsx
+++ frontend/src/pages/PrivacyPolicy/Bottom.styled.jsx
@@ -2,36 +2,46 @@ import styled from 'styled-components';
 
 export const StyledBottom = styled.div`
 	background-color: #eef1fc;
-	height: 400px;
-	margin-botoom: 20px;
-	text-align: center;
+	height: 468px;
+	margin-top: 91px;
+	display: flex ;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 
 	h2 {
-		padding-top: 30px;
-		color: #2b2c34;
-		font-size: 45px;
-		font-style: bold;
-		line-height: 66px;
-	}
-
-	button {
-		background: ${(props) => (props.outlined ? '#fff' : '#233BA9')};
-		padding: 10px 40px;
-		border-radius: 6px;
-		color: ${(props) => (props.outlined ? '#233BA9' : '#fff')};
-		border: ${(props) =>
-			props.outlined ? '1px #233BA9 solid' : '1px #233BA9 solid'};
-		font-size: 1rem;
 		font-style: normal;
-		font-weight: 400;
+		font-weight: 700;
+		font-size: 45px;
+		line-height: 66px;
 		text-align: center;
-		margin-top: 50px;
+		color: #2B2C34;
+		padding-bottom: 40px;
+	}
+
+	
+	a{
+		font-style: normal;
+		font-weight: 900;
+		font-size: 18px;
+		line-height: 24px;
+		text-align: center;
+		color: #FFFFFF;
+		display: flex;
+		width: 100%;
+		/* padding: 10px; */
+		justify-content: space-between;
+		align-items: center;
+		span{
+			padding-left: 5px;
+		}
 	}
 
 
 
-	@media (max-width: 1020px) {
-			display: none;
+	/* @media (max-width: 1020px) { */
+			/* display: none;
 	
-		{
+		{ */
+/* } */
 `;
