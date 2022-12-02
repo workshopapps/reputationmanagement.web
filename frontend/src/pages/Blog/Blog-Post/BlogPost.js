88439a6b90cc0719040afb33df1475157f2fd692
KhayT <tkprof4@gmail.com>
2022-12-02 06:40:20 -0800

    feat(FE-93): Added Blog Post page

--- frontend/src/pages/Blog/Blog-Post/BlogPost.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PageLayout from '../../../layout/PageLayout';
-import ErrorPage from '../../ErrorPage';
-
-const BlogPost = () => {
-	return <ErrorPage />;
-};
-
-export default BlogPost;
