722765f6e502c66a4cba5907ae73890c7aa5bc59
dyCodes <yusufdaudu51@gmail.com>
2022-12-04 00:05:37 +0100

    feat: Implement API endpoint for preferences page

--- frontend/src/components/Settings/Main.jsx
+++ frontend/src/components/Settings/Main.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import React, { useEffect, useState } from 'react';
 import Accounts from './Accounts';
 import Notifications from './Notifications';
 import Preferences from './Preferences';
@@ -17,6 +19,14 @@ export default function Main() {
 	const toggleTab = (index) => {
 		setTab(index);
 	};
+
+	const [userLanguage, setUserLanguage] = useState('english');
+
+	useEffect(() => {
+		CustomApiPrivate.get('/customer/language').then((res) => {
+			setUserLanguage(res.data);
+		});
+	}, []);
 
 	return (
 		<StyledParent>
@@ -56,9 +66,23 @@ export default function Main() {
 			</div>
 
 			{tab === 1 && <Accounts />}
-			{tab === 2 && <Preferences />}
+			{tab === 2 && (
+				<Preferences
+					userLanguage={userLanguage}
+					setUserLanguage={setUserLanguage}
+				/>
+			)}
 			{tab === 3 && <Notifications />}
 			{tab === 4 && <Security />}
 		</StyledParent>
 	);
 }
+
+export const CustomApiPrivate = axios.create({
+	baseURL: process.env.REACT_APP_API_URL,
+	// withCredentials: true,
+	headers: {
+		'Content-Type': 'application/json',
+		Authorization: 'Bearer ' + Cookies.get('reputeAccessToken'),
+	},
+});
--- frontend/src/components/Settings/Preferences.jsx
+++ frontend/src/components/Settings/Preferences.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import useAppContext from '../../hooks/useAppContext';
+import { CustomApiPrivate } from './Main';
 import {
 	styleClass,
 	StyledButton,
@@ -6,22 +8,48 @@ import {
 	StyledTab,
 } from './Settings.styled';
 
-function Preferences() {
+function Preferences({ userLanguage, setUserLanguage }) {
+	const languages = ['english', 'german', 'russian', 'chinese'];
+	const {
+		setRequestFailed,
+		setRequestSuccess,
+		setErrMessage,
+		setSuccessMessage,
+	} = useAppContext();
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		CustomApiPrivate.post('/customer/language?language=' + userLanguage)
+			.then((res) => {
+				setUserLanguage(res.data);
+				setSuccessMessage('Update successful');
+				setRequestSuccess(true);
+			})
+			.catch(function (error) {
+				setErrMessage('Update failed');
+				setRequestFailed(true);
+			});
+	};
+
 	return (
 		<StyledTab>
-			<form className="py-2">
+			<form onSubmit={onSubmit} className={styleClass.form}>
 				<div className={styleClass.inputGroup}>
 					<label htmlFor="first" className="md:w-1/3 font-semibold">
 						Preferred Language
 					</label>
 
-					<select className={styleClass.selectClass}>
-						<option value="English" selected>
-							English
-						</option>
-						<option value="FR">French</option>
-						<option value="ES">Spanish</option>
-						<option value="JP">Japanese</option>
+					<select
+						className={styleClass.selectClass}
+						value={userLanguage}
+						onChange={(e) => setUserLanguage(e.target.value)}
+						required
+					>
+						{languages.map((item) => (
+							<option key={item} value={item}>
+								{item}
+							</option>
+						))}
 					</select>
 				</div>
 
@@ -41,8 +69,8 @@ function Preferences() {
 				</div>
 
 				<div className="my-14 flex justify-end">
-					<StyledButtonText>Discard</StyledButtonText>
-					<StyledButton>Save Changes</StyledButton>
+					<StyledButtonText type="reset">Discard</StyledButtonText>
+					<StyledButton type="submit">Save Changes</StyledButton>
 				</div>
 			</form>
 		</StyledTab>
