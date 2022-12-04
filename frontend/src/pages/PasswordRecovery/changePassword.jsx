e38952766b16b06a78af4c55ca7e1e67ca24dab9
SolomonWole <solomonwolemail@gmail.com>
2022-12-04 22:42:40 +0100

    fix(FE-98): Fix toast alignment

--- frontend/src/pages/PasswordRecovery/changePassword.jsx
+++ frontend/src/pages/PasswordRecovery/changePassword.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 import ResetSuccess from '../../modal/resetpasswordsuccess/resetpasswordsuccessful';
 import { toast } from 'react-toastify';
 // import { even } from 'prelude-ls';
 import Api from '../../api/axios';
-import bg_img from '../../assets/images/woman_on_phone.png'
-import repute_logo from '../../assets/images/repute_logo.png'
-import { Link } from "react-router-dom"
-
+import bg_img from '../../assets/images/woman_on_phone.png';
+import repute_logo from '../../assets/images/repute_logo.png';
+import { Link } from 'react-router-dom';
 
 export default function ChangePassword() {
 	const [token, setToken] = React.useState('');
 	const [password, setPassword] = React.useState('');
 	const [confirmPassword, setConfirmPassword] = React.useState('');
 	const [passwordShown] = React.useState(false);
-	const [confirmPasswordShown ] = React.useState(false);
+	const [confirmPasswordShown] = React.useState(false);
 	// const [passwordShown, setPasswordShown] = React.useState(false);
 	// const [confirmPasswordShown, setConfirmPasswordShown] = React.useState(false);
 	const [resetPasswordModal, setResetPasswordModal] = React.useState(false);
 	const [showPasswordModal, setShowPasswordModal] = React.useState(false);
-
-	
 
 	// const togglePassword = (event) => {
 	// 	event.preventDefault();
@@ -49,7 +46,10 @@ export default function ChangePassword() {
 		const email = localStorage.getItem('auth');
 
 		if (password !== confirmPassword) {
-			toast.error('Password does not match');
+			toast.error('Password does not match', {
+				position: 'top-left',
+				autoClose: 2000,
+			});
 		}
 		try {
 			const response = Api.post('/auth/reset-password', {
@@ -59,26 +59,28 @@ export default function ChangePassword() {
 				confirmPassword: confirmPassword,
 			});
 
-            if ((await response).status === 200) {
-                localStorage.clear('auth', email);
-                setToken('');
-                setPassword('');
-                setConfirmPassword('');
-                setShowPasswordModal(true);
-                setResetPasswordModal(true);
+			if ((await response).status === 200) {
+				localStorage.clear('auth', email);
+				setToken('');
+				setPassword('');
+				setConfirmPassword('');
+				setShowPasswordModal(true);
+				setResetPasswordModal(true);
 			}
 		} catch (err) {
-            setShowPasswordModal(true);
-            setResetPasswordModal(false);
-        }
-    }
+			setShowPasswordModal(true);
+			setResetPasswordModal(false);
+		}
+	};
 
-  return (
-    <StyledParent>
-        <StyledSection onSubmit={handleSubmit}>
-            <StyledLogo src={repute_logo} alt="repute logo"/>
-            <StyledHeader>Reset Password</StyledHeader>
-            <StyledParagraph>Please enter and confirm your new password</StyledParagraph>
+	return (
+		<StyledParent>
+			<StyledSection onSubmit={handleSubmit}>
+				<StyledLogo src={repute_logo} alt="repute logo" />
+				<StyledHeader>Reset Password</StyledHeader>
+				<StyledParagraph>
+					Please enter and confirm your new password
+				</StyledParagraph>
 
 				<StyledForm style={{ opacity: showPasswordModal ? 0.4 : 1 }}>
 					<label htmlFor="token">Enter token</label>
@@ -96,7 +98,7 @@ export default function ChangePassword() {
 					<label htmlFor="password">Enter new password</label>
 					<StyledFormInput>
 						<StyledInput
-							type= {passwordShown ? 'text' : 'password'}
+							type={passwordShown ? 'text' : 'password'}
 							placeholder="Max of 8 characters"
 							required
 							onChange={handleChange}
@@ -107,7 +109,7 @@ export default function ChangePassword() {
 					<label htmlFor="password">Confirm the new password</label>
 					<StyledFormInput>
 						<StyledInput
-							type= {confirmPasswordShown ? 'text' : 'password'}
+							type={confirmPasswordShown ? 'text' : 'password'}
 							placeholder="Ensure it is the same"
 							required
 							onChange={handleConfirm}
@@ -143,24 +145,35 @@ export default function ChangePassword() {
 						</button>
 					</StyledFormInput> */}
 
-                <StyledSubmit type="submit">Sign in</StyledSubmit>
-            </StyledForm>
-            <StyledModal>
-                {showPasswordModal && <ResetSuccess resetPasswordModal={resetPasswordModal} />}
-            </StyledModal>
-            
-        </StyledSection>
-        <StyledDiv>
-                <StyledImg src={bg_img} className='background' alt="woman on the phone calling"  />
-                <StyledImgText>Welcome back, <span><Link to="/blog">click here</Link></span> to check out our new updates</StyledImgText>
-        </StyledDiv>
-    </StyledParent>        
-  )
+					<StyledSubmit type="submit">Sign in</StyledSubmit>
+				</StyledForm>
+				<StyledModal>
+					{showPasswordModal && (
+						<ResetSuccess resetPasswordModal={resetPasswordModal} />
+					)}
+				</StyledModal>
+			</StyledSection>
+			<StyledDiv>
+				<StyledImg
+					src={bg_img}
+					className="background"
+					alt="woman on the phone calling"
+				/>
+				<StyledImgText>
+					Welcome back,{' '}
+					<span>
+						<Link to="/blog">click here</Link>
+					</span>{' '}
+					to check out our new updates
+				</StyledImgText>
+			</StyledDiv>
+		</StyledParent>
+	);
 }
 
 const StyledParent = styled.div`
-    display: flex;
-`
+	display: flex;
+`;
 
 const StyledSection = styled.section`
 	position: relative;
@@ -168,89 +181,93 @@ const StyledSection = styled.section`
 	max-width: 600px;
 	margin: 50px auto;
 
-    @media (max-width: 500px) {
-        margin: 5px auto;
-    }
-`
+	@media (max-width: 500px) {
+		margin: 5px auto;
+	}
+`;
 
 const StyledDiv = styled.div`
-    width: 50%;   
-    height: 100vh;
-    position: relative;
+	width: 50%;
+	height: 100vh;
+	position: relative;
 
-    @media (max-width: 600px){
-        display: none;
-    }
-`
+	@media (max-width: 600px) {
+		display: none;
+	}
+`;
 
 const StyledLogo = styled.img`
-    margin: 0 auto;
-    margin-bottom: 60px;
+	margin: 0 auto;
+	margin-bottom: 60px;
 
-    @media (max-width: 500px){
-            margin-top: 60px;
-        }
-`
+	@media (max-width: 500px) {
+		margin-top: 60px;
+	}
+`;
 
 const StyledImg = styled.img`
-    width: 100%;
-    height: 100%;
-`
+	width: 100%;
+	height: 100%;
+`;
 
 const StyledImgText = styled.p`
-    position: absolute;
-    bottom: 9%;
-    background: linear-gradient(180deg, rgba(245, 245, 245, 0.1764) -22.33%, rgba(245, 245, 245, 0.1904) 77.67%);
-    mix-blend-mode: normal;
-    border: 0.85625px solid rgba(255, 255, 255, 0.3);
-    box-shadow: 0px 3.425px 20.55px rgba(0, 0, 0, 0.25);
-    backdrop-filter: blur(12.8438px);
-    border-radius: 34.25px;
-    padding: 50px;
-    width: 60%;
-    left: 50%;
-    transform: translate(-50%, 0);
-    color: #fff;
+	position: absolute;
+	bottom: 9%;
+	background: linear-gradient(
+		180deg,
+		rgba(245, 245, 245, 0.1764) -22.33%,
+		rgba(245, 245, 245, 0.1904) 77.67%
+	);
+	mix-blend-mode: normal;
+	border: 0.85625px solid rgba(255, 255, 255, 0.3);
+	box-shadow: 0px 3.425px 20.55px rgba(0, 0, 0, 0.25);
+	backdrop-filter: blur(12.8438px);
+	border-radius: 34.25px;
+	padding: 50px;
+	width: 60%;
+	left: 50%;
+	transform: translate(-50%, 0);
+	color: #fff;
 
-    span{
-        color: #F16F04;
-        
-        &:hover {
-            cursor: pointer;
-        }
-    }
-`
+	span {
+		color: #f16f04;
+
+		&:hover {
+			cursor: pointer;
+		}
+	}
+`;
 const StyledHeader = styled.h1`
-text-align: center;
-color: #2B2C34;
-font-size: 40px;
-font-weight: 700;
-padding: 5px;
+	text-align: center;
+	color: #2b2c34;
+	font-size: 40px;
+	font-weight: 700;
+	padding: 5px;
 
-@media (max-width: 500px) {
-        margin-top: 5px;
-        font-size: 24px;
-    }
-`
+	@media (max-width: 500px) {
+		margin-top: 5px;
+		font-size: 24px;
+	}
+`;
 
 const StyledParagraph = styled.p`
-font-weight: 400;
-text-align: center;
-font-size: 16px;
-margin-bottom: 40px;
+	font-weight: 400;
+	text-align: center;
+	font-size: 16px;
+	margin-bottom: 40px;
 
-    @media (max-width: 500px){
-        font-size: 15px;
-        width: 90%;
-        margin: 0 auto 40px;
-    }
-`
+	@media (max-width: 500px) {
+		font-size: 15px;
+		width: 90%;
+		margin: 0 auto 40px;
+	}
+`;
 
 const StyledForm = styled.form`
-display: flex;
-flex-direction: column;
-padding: 0px 10px;
-/* {showPasswordModal && opacity: 0.5}; */
+	display: flex;
+	flex-direction: column;
+	padding: 0px 10px;
+	/* {showPasswordModal && opacity: 0.5}; */
 
 	@media (max-width: 500px) {
 		margin-bottom: 50px;
@@ -258,9 +275,9 @@ padding: 0px 10px;
 `;
 
 const StyledFormInput = styled.form`
-    display: flex;
-    flex-direction: column;
-    position: relative;
+	display: flex;
+	flex-direction: column;
+	position: relative;
 
 	button {
 		position: absolute;
@@ -270,20 +287,20 @@ const StyledFormInput = styled.form`
 `;
 
 const StyledModal = styled.div`
-    position: absolute;
-    top: 45%;
-    left: 50%;
-    transform: translate(-50%, 0);
-    width: 80%;
-`
+	position: absolute;
+	top: 45%;
+	left: 50%;
+	transform: translate(-50%, 0);
+	width: 80%;
+`;
 
 const StyledInput = styled.input`
-background: #FFFFFF;
-border: 1px solid #E8E8E8;
-border-radius: 8px;
-padding: 8px 20px;
-margin-top: 15px;
-margin-bottom: 20px;
+	background: #ffffff;
+	border: 1px solid #e8e8e8;
+	border-radius: 8px;
+	padding: 8px 20px;
+	margin-top: 15px;
+	margin-bottom: 20px;
 
 	&:focus {
 		outline: 1px solid #233ba9;
--- frontend/src/pages/PasswordRecovery/passwordRecovery.jsx
+++ frontend/src/pages/PasswordRecovery/passwordRecovery.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
-import bg_img from '../../assets/images/woman_on_phone.png'
-import repute_logo from '../../assets/images/repute_logo.png'
-import arrow_left from '../../assets/images/password_arrow_left.png'
-import authentication_icon from '../../assets/images/authenticate_img.png'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import bg_img from '../../assets/images/woman_on_phone.png';
+import repute_logo from '../../assets/images/repute_logo.png';
+import arrow_left from '../../assets/images/password_arrow_left.png';
+import authentication_icon from '../../assets/images/authenticate_img.png';
 import { toast, ToastContainer } from 'react-toastify';
 import Api from '../../api/axios';
-
 
 export default function PasswordRecovery() {
 	const navigate = useNavigate();
@@ -19,231 +18,248 @@ export default function PasswordRecovery() {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		
 
 		try {
 			const response = Api.post('/auth/forgotpassword', {
 				emailAddress: email,
 			});
 			if ((await response).status === 200) {
-				toast.success('Reset link sent to email address');
-                localStorage.setItem('auth', email);
+				toast.success('Reset link sent to email address', {
+					position: 'top-left',
+					autoClose: 2000,
+				});
+				localStorage.setItem('auth', email);
 				setEmail('');
-                navigate('/password-recovery/change');
+				navigate('/password-recovery/change');
 			}
 		} catch (err) {
-            toast.error('Email does not exist', {
-                position: "top-left",
-                autoClose: 2000,
-            });
+			toast.error('Email does not exist', {
+				position: 'top-left',
+				autoClose: 2000,
+			});
 		}
 	};
 	return (
 		<StyledParent>
 			<StyledSection onSubmit={handleSubmit}>
-                <StyledLogo src={repute_logo} alt="repute logo"/>
-                <div>
-                    <Link to="/login">
-                        <StyledIcon src={arrow_left} alt="arrow_left_icon" />
-                    </Link>
-                    <StyledHeader>Forgot Password?</StyledHeader>
-                </div>
-                
-                <StyledAuth src={authentication_icon} alt="authentication icon" />
-                <StyledParagraph>Please enter the email address you used when you joined and we’ll send you instructions to reset your password.</StyledParagraph>
+				<StyledLogo src={repute_logo} alt="repute logo" />
+				<div>
+					<Link to="/login">
+						<StyledIcon src={arrow_left} alt="arrow_left_icon" />
+					</Link>
+					<StyledHeader>Forgot Password?</StyledHeader>
+				</div>
 
-                <StyledForm>
-                    <label htmlFor="email">Email</label>
-                    <StyledInput 
-                    type="email" 
-                    placeholder="Marksons@gmail.com" 
-                    required 
-                    onChange={handleChange}
-                    value={email}/>
+				<StyledAuth src={authentication_icon} alt="authentication icon" />
+				<StyledParagraph>
+					Please enter the email address you used when you joined and we’ll send
+					you instructions to reset your password.
+				</StyledParagraph>
 
-                    <StyledSubmit type="submit">Sign in</StyledSubmit>
-                </StyledForm>
-        </StyledSection>
-        <StyledDiv>
-                <StyledImg src={bg_img} className='background' alt="woman on the phone calling"  />
-                <StyledImgText>Welcome back, <span><Link to="/blog">click here</Link></span> to check out our new updates</StyledImgText>
-        </StyledDiv>
-        <ToastContainer />
-    </StyledParent>
-  )
+				<StyledForm>
+					<label htmlFor="email">Email</label>
+					<StyledInput
+						type="email"
+						placeholder="Marksons@gmail.com"
+						required
+						onChange={handleChange}
+						value={email}
+					/>
+
+					<StyledSubmit type="submit">Sign in</StyledSubmit>
+				</StyledForm>
+			</StyledSection>
+			<StyledDiv>
+				<StyledImg
+					src={bg_img}
+					className="background"
+					alt="woman on the phone calling"
+				/>
+				<StyledImgText>
+					Welcome back,{' '}
+					<span>
+						<Link to="/blog">click here</Link>
+					</span>{' '}
+					to check out our new updates
+				</StyledImgText>
+			</StyledDiv>
+			<ToastContainer />
+		</StyledParent>
+	);
 }
 
 const StyledParent = styled.div`
-    display: flex;
-`
+	display: flex;
+`;
 const StyledSection = styled.section`
-max-width: 600px;
-margin: 50px auto;
+	max-width: 600px;
+	margin: 50px auto;
 
-    div {
-        display: flex;
-        flex-direction: column;
-        gap: 30px;
-        align-items: center;
+	div {
+		display: flex;
+		flex-direction: column;
+		gap: 30px;
+		align-items: center;
 
-        @media (max-width: 500px){
-            flex-direction: row;
-            margin-bottom: 40px;
+		@media (max-width: 500px) {
+			flex-direction: row;
+			margin-bottom: 40px;
+		}
+	}
 
-        }
-    }
-
-    @media (max-width: 500px) {
-        margin: 5px auto;
-    }
-`
+	@media (max-width: 500px) {
+		margin: 5px auto;
+	}
+`;
 const StyledLogo = styled.img`
-    margin: 0 auto;
-    margin-bottom: 60px;
+	margin: 0 auto;
+	margin-bottom: 60px;
 
-    @media (max-width: 500px){
-            display: none;
-
-        }
-`
+	@media (max-width: 500px) {
+		display: none;
+	}
+`;
 const StyledAuth = styled.img`
-    display: none;
+	display: none;
 
-    @media (max-width: 500px){
-        display: block;
-        width: 150px;
-        height: 150px;
-        margin: 0 auto 20px;
-    }
-`
+	@media (max-width: 500px) {
+		display: block;
+		width: 150px;
+		height: 150px;
+		margin: 0 auto 20px;
+	}
+`;
 
 const StyledIcon = styled.img`
-    display: none;
+	display: none;
 
-    &:hover {
-            border: 1px solid #F16F04;
-            cursor: pointer;
-        }
+	&:hover {
+		border: 1px solid #f16f04;
+		cursor: pointer;
+	}
 
-    @media (max-width: 500px){
-        display: inline-block;
-        border-radius: 50%;
-        border: 1px solid #BCBDBE;
-        width: 40px;
-        height: 40px;
-        margin-left: 30px;
-    }
-`
+	@media (max-width: 500px) {
+		display: inline-block;
+		border-radius: 50%;
+		border: 1px solid #bcbdbe;
+		width: 40px;
+		height: 40px;
+		margin-left: 30px;
+	}
+`;
 
 const StyledDiv = styled.div`
-    width: 50%;   
-    height: 100vh;
-    position: relative;
+	width: 50%;
+	height: 100vh;
+	position: relative;
 
-    @media (max-width: 600px){
-        display: none;
-    }
-`
+	@media (max-width: 600px) {
+		display: none;
+	}
+`;
 
 const StyledImg = styled.img`
-    width: 100%;
-    height: 100%;
-`
+	width: 100%;
+	height: 100%;
+`;
 
 const StyledImgText = styled.p`
-    position: absolute;
-    bottom: 9%;
-    background: linear-gradient(180deg, rgba(245, 245, 245, 0.1764) -22.33%, rgba(245, 245, 245, 0.1904) 77.67%);
-    mix-blend-mode: normal;
-    border: 0.85625px solid rgba(255, 255, 255, 0.3);
-    box-shadow: 0px 3.425px 20.55px rgba(0, 0, 0, 0.25);
-    backdrop-filter: blur(12.8438px);
-    border-radius: 34.25px;
-    padding: 50px;
-    width: 60%;
-    left: 50%;
-    transform: translate(-50%, 0);
-    color: #fff;
+	position: absolute;
+	bottom: 9%;
+	background: linear-gradient(
+		180deg,
+		rgba(245, 245, 245, 0.1764) -22.33%,
+		rgba(245, 245, 245, 0.1904) 77.67%
+	);
+	mix-blend-mode: normal;
+	border: 0.85625px solid rgba(255, 255, 255, 0.3);
+	box-shadow: 0px 3.425px 20.55px rgba(0, 0, 0, 0.25);
+	backdrop-filter: blur(12.8438px);
+	border-radius: 34.25px;
+	padding: 50px;
+	width: 60%;
+	left: 50%;
+	transform: translate(-50%, 0);
+	color: #fff;
 
-    span{
-        color: #F16F04;
-        
-        &:hover {
-            cursor: pointer;
-        }
-    }
-`
+	span {
+		color: #f16f04;
+
+		&:hover {
+			cursor: pointer;
+		}
+	}
+`;
 const StyledHeader = styled.h1`
-text-align: center;
-color: #2B2C34;
-font-size: 40px;
-font-weight: 700;
-padding: 5px;
+	text-align: center;
+	color: #2b2c34;
+	font-size: 40px;
+	font-weight: 700;
+	padding: 5px;
 
-@media (max-width: 500px) {
-        margin-top: 5px;
-        font-size: 24px;
-    }
-`
+	@media (max-width: 500px) {
+		margin-top: 5px;
+		font-size: 24px;
+	}
+`;
 
 const StyledParagraph = styled.p`
-font-weight: 400;
-text-align: center;
-font-size: 16px;
-margin-bottom: 40px;
+	font-weight: 400;
+	text-align: center;
+	font-size: 16px;
+	margin-bottom: 40px;
 
-    @media (max-width: 500px){
-        width: 80%;
-        margin: 0 auto 40px;
-    }
-`
+	@media (max-width: 500px) {
+		width: 80%;
+		margin: 0 auto 40px;
+	}
+`;
 
 const StyledForm = styled.form`
-display: flex;
-flex-direction: column;
-padding: 0 10px;
+	display: flex;
+	flex-direction: column;
+	padding: 0 10px;
 
-    label {
-        @media (max-width: 500px){
-            width: 80%;
-            margin: 0 auto;
-        }
-    }
+	label {
+		@media (max-width: 500px) {
+			width: 80%;
+			margin: 0 auto;
+		}
+	}
 
-    @media (max-width: 500px) {
-        margin-bottom: 50px;
-    }
-`
+	@media (max-width: 500px) {
+		margin-bottom: 50px;
+	}
+`;
 
 const StyledInput = styled.input`
-background: #FFFFFF;
-border: 1px solid #E8E8E8;
-border-radius: 8px;
-padding: 8px 20px;
-margin-top: 15px;
+	background: #ffffff;
+	border: 1px solid #e8e8e8;
+	border-radius: 8px;
+	padding: 8px 20px;
+	margin-top: 15px;
 
-&:focus {
-    outline: 1px solid #233ba9;
-}
+	&:focus {
+		outline: 1px solid #233ba9;
+	}
 
-@media (max-width: 500px){
-    width: 80%;
-    margin: 15px auto 0;
-}
-`
+	@media (max-width: 500px) {
+		width: 80%;
+		margin: 15px auto 0;
+	}
+`;
 
 const StyledSubmit = styled.button`
-background-color: #233ba9;
-width: 100%;
-margin-top: 20px;
-padding: 10px 20px;
-color: #fff;
-border-radius: 15px;
-text-align: center;
+	background-color: #233ba9;
+	width: 100%;
+	margin-top: 20px;
+	padding: 10px 20px;
+	color: #fff;
+	border-radius: 15px;
+	text-align: center;
 
-@media (max-width: 500px){
-        width: 80%;
-        margin: 60px auto 0;
-    }
-`
-
+	@media (max-width: 500px) {
+		width: 80%;
+		margin: 60px auto 0;
+	}
+`;
