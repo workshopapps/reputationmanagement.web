86ee8cd2949062c7da2f9733133696b4465e2ab1
simeon ehioze <simeonehioze@gmail.com>
2022-12-04 09:43:22 +0100

    bug(FE-102): resolved conflicts

--- frontend/src/components/Reusables/Sidebar.jsx
+++ frontend/src/components/Reusables/Sidebar.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import closeBtn from '../../assets/images/Dashboard/x.svg';
 import logo from '../../assets/images/Dashboard/logo.png';
-import { DashboardIcon, SettingsIcon, SignoutIcon } from '../Dashboard/Icons';
+import {
+	DashboardIcon,
+	SettingsIcon,
+	SignoutIcon,
+	ProfileIcon,
+} from '../Dashboard/Icons';
 import { StyledSidebar } from '../Styles/SideBar.styled';
-import styled from 'styled-components';
 import useLogoutConfirmation from '../../hooks/useLogoutConfirmation';
 import LogoutConfirmationModal from '../../modal/logoutConfirmationModal';
-import logout from "../../assets/images/logout.svg"
 
 const Sidebar = (props) => {
-	
-
-	const {isShowing, toggle} = useLogoutConfirmation();
+	const { isShowing, toggle } = useLogoutConfirmation();
 
 	return (
 		<StyledSidebar className={props.className}>
@@ -24,7 +25,7 @@ const Sidebar = (props) => {
 			<ul>
 				<li>
 					<NavLink to="/dashboard">
-						<DashboardIcon  />
+						<DashboardIcon />
 						Dashboard
 					</NavLink>
 				</li>
@@ -41,38 +42,18 @@ const Sidebar = (props) => {
 						Settings
 					</NavLink>
 				</li>
-			</ul>
-			<ul style={{ maxHeight: '50px'}}>
+
 				<li>
-					<StyledLogoutButton onClick={toggle}>
-					<img src={logout} alt="logout"/>Logout
-					</StyledLogoutButton>
+					<button onClick={toggle}>
+						<SignoutIcon />
+						Signout
+					</button>
 				</li>
-
-				<LogoutConfirmationModal isShowing={isShowing} hide={toggle} />
 			</ul>
 
-			
-
+			<LogoutConfirmationModal isShowing={isShowing} hide={toggle} />
 		</StyledSidebar>
 	);
 };
 
-const StyledLogoutButton = styled.div`
-	//position: absolute;
-	display: flex;
-	align-items: center;
-	color: gray;
-	padding: 10px 55px;
-	max-width: 100%;
-	margin-left: 2rem;
-	border-radius: 3px;
-	border: none;
-	text-align: center;
-	cursor: pointer;
-
-	img {
-		margin-right: 5px;
-	}
-`;
 export default Sidebar;
