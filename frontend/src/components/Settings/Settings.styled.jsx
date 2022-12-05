e7d45009ce75763cb0bbfecacee4d1bc55f1a316
dyCodes <yusufdaudu51@gmail.com>
2022-12-05 18:30:57 +0100

    feat: Implement notifications API endpoint

--- frontend/src/components/Settings/Settings.styled.jsx
+++ frontend/src/components/Settings/Settings.styled.jsx
@@ -31,12 +31,13 @@ export const StyledTab = styled.div`
 	}
 `;
 
-export const StyledButtonOutline = styled.button`
+export const StyledButtonOutline = styled.div`
 	color: #233ba9;
 	border: 1px solid #233ba9;
 	padding: 10px 25px;
 	border-radius: 3px;
 	text-align: center;
+	cursor: pointer;
 `;
 
 export const StyledButton = styled.button`
