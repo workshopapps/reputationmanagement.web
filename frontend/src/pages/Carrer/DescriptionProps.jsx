f3322b2ab3e45dfa3e035a2d3930ebce98d784d1
Princessayomide53 <94536831+Princessayomide53@users.noreply.github.com>
2022-12-08 05:28:56 -0500

    implemented the api call in modal

--- frontend/src/pages/Carrer/DescriptionProps.jsx
+++ frontend/src/pages/Carrer/DescriptionProps.jsx
@@ -11,19 +11,21 @@ const StyledHeader = styled.h1`
 	color: #233ba9;
 	opacity: 0.7;
 	font-size: 2rem;
-	padding: 4px 0;
+	
+	margin-top: -15px;
+	font: bold;
 `;
-const StyledImg = styled.img`
-	width: 100px;
-	height: 100px;
-`;
+// const StyledImg = styled.img`
+// 	width: 100px;
+// 	height: 100px;
+// `;
 
 function DescriptionProps({ src, title, description }) {
 	return (
 		<div className="descriptions flex flex-col px-6 py-4 max-w-md">
-			<StyledImg  />
+			{/* <StyledImg  /> */}
 			<StyledHeader className="font-bold">{title}</StyledHeader>
-			<h4>{description}</h4>
+			{/* <h4>{description}</h4> */}
 		</div>
 	);
 	// src={src} alt=""
