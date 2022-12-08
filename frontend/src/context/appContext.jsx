788026070863a64b075b8ff3934876b0a51e02d8
John <johnorisanwo@gmail.com>
2022-12-09 00:02:26 +0100

    updating request successful modal

--- frontend/src/context/appContext.jsx
+++ frontend/src/context/appContext.jsx
@@ -5,7 +5,7 @@ export const AppContext = createContext();
 
 const AppProvider = ({children}) => {
     const [ someState, setSomeState ] = useState(0);
-    const [ requestSuccessfulModalActive, setRequestSuccessfulModalActive ] = useState(true)
+    const [ requestSuccessfulModalActive, setRequestSuccessfulModalActive ] = useState(false)
     const [ allRequests, setAllRequests ] = useState(TableDatas);
     const [ errMessage, setErrMessage ] = useState('Sign up Failed');
     const [ successMessage, setSuccessMessage ] = useState('Request succcessful');
