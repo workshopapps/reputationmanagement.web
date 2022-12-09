b2f295f0887ea41b72e5300d0b85f9bfcf7b5828
SolomonWole <solomonwolemail@gmail.com>
2022-12-09 16:09:42 +0100

    imported react

--- frontend/src/pages/LandingPage/index2.jsx
+++ frontend/src/pages/LandingPage/index2.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageLayout from '../../layout/PageLayout';
 import styled from 'styled-components';
