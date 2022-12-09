eb21313909c5cb66261e809bb05743c76eba8164
simeon ehioze <simeonehioze@gmail.com>
2022-12-09 10:33:30 +0100

    removed integration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,7 +13,7 @@ import { BrowserTracing } from "@sentry/tracing";
 Sentry.init({
     dsn: "https://acbed01f3af04154a1206f028d3d9b79@o4504259490873344.ingest.sentry.io/4504286581882880",
     integrations: [
-        new Integrations.BrowserTracing({
+        new BrowserTracing({
         tracingOrigins: ["localhost", "api.repute.hng.tech"]})],
     tracesSampleRate: 1.0
 });
