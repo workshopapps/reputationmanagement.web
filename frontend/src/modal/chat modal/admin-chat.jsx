c9b649cecfa89f34e9817fd2fc44130faef6476f
John <johnorisanwo@gmail.com>
2022-12-11 15:03:26 +0100

    adding live chat

--- frontend/src/modal/chat modal/admin-chat.jsx
+++ frontend/src/modal/chat modal/admin-chat.jsx
@@ -1,45 +1,45 @@
 import Talk from 'talkjs';
 import { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
+import DEFAULTAVATAR from './default-avatar.jpeg';
 import useAppContext from '../../hooks/useAppContext';
 
-const ChatModal = () => {
+const AdminChatModal = () => {
   const chatboxEl = useRef();
-  const { setChatModalActive } = useAppContext();
+  const { setChatModalActive } = useAppContext()
   // wait for TalkJS to load
   const [talkLoaded, markTalkLoaded] = useState(false);
 
-  const email = localStorage.getItem('auth') || 'user@mail.com';
-
+  const id = Math.floor(Math.random())*969287
+  if (!localStorage.getItem('chatId')){
+    localStorage.setItem('chatId', id)
+  }
   useEffect(() => {
     Talk.ready.then(() => markTalkLoaded(true));
 
     if (talkLoaded) {
-      const currentUser = new Talk.User({
+        const me = new Talk.User({
+          id: "1",
+          name: "Idaresit",
+          email: "idaresit@gmail.com",
+          photoUrl: "https://talkjs.com/images/avatar-1.jpg",
+          welcomeMessage: "Welcome to Repute! How can I help you",
+          role: "admin" // <-- that's the one!
+      });
+      const otherUser = new Talk.User({
         id: 2,
         name: `Customer`,
         photoUrl: "https://i1.wp.com/www.neoparent.be/wp-content/uploads/2018/04/cropped-default_avatar-1.png?ssl=1",
-        email: email,
         role: 'default',
       });
-
-      const otherUser = new Talk.User({
-        id: '1',
-        name: 'Idaresit',
-        email: 'idaresit@gmail.com',
-        welcomeMessage: 'Welcome to Repute! How can I help you',
-        photoUrl: "https://talkjs.com/images/avatar-2.jpg",
-        role: 'admin',
-      });
-
       const session = new Talk.Session({
         appId: 'tpjbDjwE',
-        me: currentUser,
+        me: me,
       });
 
-      const conversationId = Talk.oneOnOneId(currentUser, otherUser);
+      const conversationId = Talk.oneOnOneId(me, otherUser);
       const conversation = session.getOrCreateConversation(conversationId);
-      conversation.setParticipant(currentUser);
+      conversation.setParticipant(me);
       conversation.setParticipant(otherUser);
 
       const chatbox = session.createChatbox();
@@ -52,7 +52,7 @@ const ChatModal = () => {
 
   return (
     <StyledChatModal>
-      <p onClick={() => setChatModalActive(false)} style={{ cursor: 'pointer'}}>close</p>
+      <p onClick={() => setChatModalActive(false)} style={{ cursor: 'pointer'}}>X</p>
       <div ref={chatboxEl}></div>
     </StyledChatModal>
   )
@@ -68,15 +68,14 @@ const StyledChatModal = styled.div`
   box-shadow: -20px -20px 50px 10px rgba(0,0,0,0.2);
   display: flex;
   flex-direction: column;
-  background-color: rgba(0,0,0,0.2);
   p{
     align-self: flex-end;
-    font-size: 18px;
-    font-weight: 300;
+    font-size: 24px;
+    font-weight: 700;
     padding: 5px 10px;
   }
   div{
     height: 600px;
   }
 `;
-export default ChatModal;
+export default AdminChatModal;
