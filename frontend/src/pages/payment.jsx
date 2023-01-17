0c08a9959b6245c1be4b9320744dd754551e3e4a
dyCodes <yusufdaudu51@gmail.com>
2023-01-17 19:16:17 +0100

    feat/FE: Integrate flutterwave payment gateway

--- frontend/src/pages/payment.jsx
+++ frontend/src/pages/payment.jsx
@@ -6,91 +6,87 @@ import Sidebar from '../components/Reusables/Sidebar';
 import WebAppNav from '../components/Reusables/WebAppNav';
 import { useState } from 'react';
 import styled from 'styled-components';
-import { PaystackButton } from "react-paystack";
+import { FlutterWaveButton, closePaymentModal } from 'flutterwave-react-v3';
 import useAxiosPrivate from '../hooks/useAxiosPrivate';
-import { useLocation,useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 
 const Payment = () => {
-	const [ cost, setCost ] = useState(0);
-	const name = localStorage.getItem('auth');
-	const [ email,setEmail ] = useState(false);
-	const [ priority, setPriority ] = useState(false);
-	const [ amount, setAmount ] = useState(5*75000)
-	const publicKey = process.env.REACT_APP_PAYMENT_KEY
+	const name = localStorage.getItem('user');
+	const email = localStorage.getItem('auth');
+	const [priority, setPriority] = useState(null);
+	const [amount, setAmount] = useState(0);
+
+	const publicKey = process.env.REACT_APP_PAYMENT_KEY;
 	const ApiPrivate = useAxiosPrivate();
-	const router = useNavigate()
+	const router = useNavigate();
 	const location = useLocation();
 	const requestId = new URLSearchParams(location.search).get('requestid');
 
 	useEffect(() => {
 		fetchDetails();
-	},[])
+	}, []);
 
-	const fetchDetails = async() => {
-		try{
+	const fetchDetails = async () => {
+		try {
 			const response = await ApiPrivate.get(`/api/review/${requestId}`);
-			setEmail(response?.data?.email);
 			setPriority(response?.data?.priority);
+		} catch (err) {
+			console.log(err);
 		}
-		catch(err){
-			console.log(err)
-		}
-	}
-	useEffect(() => {
-		setAmount(cost * 75000)
-	},[cost])
-	
+	};
+
 	useEffect(() => {
 		priority === 3
-			?
-			setCost(30)
-			:
-			priority === 2
-				?
-				setCost(20)
-				:
-				priority === 1
-					?
-					setCost(10)
-					:
-					setCost(5)
-	},[priority])
+			? setAmount(30)
+			: priority === 2
+			? setAmount(20)
+			: priority === 1
+			? setAmount(10)
+			: setAmount(5);
+	}, [priority]);
 
-	const savePayment = async() => {
-		try{
-			const response = await ApiPrivate.patch(
-				`/api/review/${requestId}`,
-				[
-					{
-						operationType: 2,
-						path: '/status',
-						op: 'replace',
-						value: 5,
-					},
-				]
-			);
-			console.log(response)
+	const savePayment = async () => {
+		try {
+			const response = await ApiPrivate.patch(`/api/review/${requestId}`, [
+				{
+					operationType: 2,
+					path: '/status',
+					op: 'replace',
+					value: 5,
+				},
+			]);
+			console.log(response);
+		} catch (err) {
+			console.log(err);
 		}
-		catch(err){
-			console.log(err)
-		}
-	}
-	const componentProps = {
-		email: email,
+	};
+
+	const fwConfig = {
+		public_key: 'FLWPUBK_TEST-899eed6270b7047fb904db68bcde3304-X',
+		tx_ref: Date.now(),
 		amount: amount,
-		metadata: {
-		  name,
+		currency: 'USD',
+		payment_options: 'card',
+		customer: {
+			email,
+			name,
 		},
-		publicKey,
-		text: "Pay Now",
-		onSuccess: () =>{
+		customizations: {
+			title: 'Repute',
+			logo: 'https://repute.legal/fav.svg',
+		},
+		text: 'Pay Now!',
+		callback: (res) => {
 			savePayment();
-			router('/dashboard')
+			closePaymentModal();
+			router('/dashboard');
 		},
-		onClose: () => alert("Payment is not completed"),
-	  }
+		onClose: () => {},
+	};
+
 	const [openMenu, setOpenMenu] = useState(false);
+
 	return (
 		<>
 			<Sidebar
@@ -107,29 +103,26 @@ const Payment = () => {
 							<h1>Your request has been completed</h1>
 
 							<h5>
-								We will start procesing your request as soon as we recieve
+								We will start processing your request as soon as we receive
 								payment. You will receive an email for payment confirmation
-								within 24hours of recieving payment
+								within 24 hours of receiving payment.
 							</h5>
 						</Section1>
 
 						<Section2>
 							<div>
 								<div>order No</div>
-
-								<Second>{requestId.substring(0,8)}</Second>
+								<Second>{requestId.substring(0, 8)}</Second>
 							</div>
 
 							<div>
 								<div>Email</div>
-
 								<Second>{email}</Second>
 							</div>
 
 							<div>
 								<div>Amount</div>
-
-								<Third>{cost}$</Third>
+								<Third>${amount}</Third>
 							</div>
 						</Section2>
 
@@ -139,7 +132,8 @@ const Payment = () => {
 							<img src={visa} alt="" />
 							<img src={verve} alt="" />
 						</Section3>
-						<PaystackButton {...componentProps} className="paystack"/>
+
+						<FlutterWaveButton {...fwConfig} className="flutterwavebutton" />
 					</div>
 				</StyledBox>
 			</StyledSection>
@@ -154,8 +148,9 @@ const StyledSection = styled.section`
 	padding-right: 100px;
 	display: flex;
 	justify-content: center;
-    align-items: center;
-  	.paystack{
+	align-items: center;
+
+	.flutterwavebutton {
 		background-color: #233ba9;
 		color: white;
 		width: 400px;
@@ -169,11 +164,11 @@ const StyledSection = styled.section`
 
 	@media (max-width: 1140px) {
 		padding-left: 0px;
-        padding-right: 0px;
+		padding-right: 0px;
 	}
 
 	@media (min-width: 1950px) {
-        height: 100vh;
+		height: 100vh;
 	}
 `;
 
@@ -187,7 +182,6 @@ const StyledBox = styled.div`
 	margin-top: 50px;
 
 	@media (max-width: 1300px) {
-		
 		padding: 50px 90px 50px 90px;
 	}
 	@media (max-width: 1140px) {
@@ -277,10 +271,10 @@ const Section3 = styled.div`
 	justify-content: center;
 	border: 1px solid black;
 	border-radius: 10px;
-	padding: 40px 57px 40px 57px;
+	padding: 30px 57px 30px 57px;
 
 	@media (max-width: 600px) {
-		padding: 40px 40px 40px 40px;
+		padding: 35px 40px;
 	}
 
 	h1 {
@@ -300,15 +294,16 @@ const Section3 = styled.div`
 	}
 
 	img {
-		width: 130px;
-		margin-right: 15px;
+		width: 105px;
+		margin-right: 30px;
 
 		&:hover {
 			cursor: pointer;
 		}
 
 		@media (max-width: 600px) {
-			width: 80px;
+			width: 70px;
+			margin-right: 15px;
 		}
 	}
 `;
